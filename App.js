59fbba7666c78a228bf2386ea546df2f8a4b2c7a
devil1716 <SSharan.s@outlook.com>
2025-12-20 01:06:29 +0530

    Fix: Resolve createDownloadResumable deprecation error and bump version to 1.3.4

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { useCameraPermissions } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
 import * as Sharing from 'expo-sharing';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import Constants from 'expo-constants';
 import * as IntentLauncher from 'expo-intent-launcher';
 import ViewShot from 'react-native-view-shot';
